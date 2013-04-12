9a4b18f06488c6a555287eee4026183d6d86d861
Adam Bien <abien@adam-bien.com>
2013-04-12 08:36:01 +0200

    Filtering works

--- src/main/java/com/airhacks/airpad/business/notes/boundary/NotesStore.java
+++ src/main/java/com/airhacks/airpad/business/notes/boundary/NotesStore.java
@@ -2,6 +2,7 @@ package com.airhacks.airpad.business.notes.boundary;
 
 import com.airhacks.airpad.business.notes.entity.Note;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
@@ -10,6 +11,7 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.ObservableMap;
+import javafx.collections.ObservableSet;
 import javax.annotation.PostConstruct;
 
 /**
@@ -20,35 +22,34 @@ public class NotesStore {
 
     private StringProperty title;
     private ObservableList<Note> notes;
-    private List<Note> matchingNotes;
+    private ObservableList<Note> filteredNotes;
 
     @PostConstruct
     public void init() {
         this.notes = FXCollections.observableArrayList();
-        this.matchingNotes = new ArrayList<>();
+        this.filteredNotes = FXCollections.observableArrayList();
         this.title = new SimpleStringProperty();
         this.title.addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> ov, String newValue, String oldValue) {
-                notes.removeAll(matchingNotes);
-                for (Note note : notes) {
+                filteredNotes.clear();
+                for (Note note : filteredNotes) {
                     if (note.matches(newValue)) {
-                        matchingNotes.add(note);
+                        filteredNotes.add(note);
                     }
                 }
-                notes.clear();
-                notes.addAll(matchingNotes);
-                matchingNotes.clear();
             }
         });
     }
 
     public void findOrCreate(String text) {
-        this.notes.add(new Note(text));
+        final Note note = new Note(text);
+        this.notes.add(note);
+        this.filteredNotes.add(note);
     }
 
     public ObservableList<Note> notes() {
-        return this.notes;
+        return this.filteredNotes;
     }
 
     public StringProperty title() {
