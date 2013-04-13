14eff4dfb67875fe6bdc0518bf0e3ccc03261a5c
Adam Bien <abien@adam-bien.com>
2013-04-13 11:55:05 +0200

    Notes are saved during replication

--- src/main/java/com/airhacks/airpad/business/notes/boundary/NotesStore.java
+++ src/main/java/com/airhacks/airpad/business/notes/boundary/NotesStore.java
@@ -24,14 +24,14 @@ import javax.annotation.PostConstruct;
  * @author adam-bien.com
  */
 public class NotesStore {
-
+    
     private IMap<String, Note> notes;
     private HazelcastInstance hazelcast;
     private ObjectProperty<Note> removed;
     private ObjectProperty<Note> added;
     private ObjectProperty<Note> updated;
     private String notesDirectory = "notes";
-
+    
     @PostConstruct
     public void init() {
         this.added = new SimpleObjectProperty<>();
@@ -45,47 +45,49 @@ public class NotesStore {
                 System.out.println("Added: " + event);
                 add(event);
             }
-
+            
             @Override
             public void entryRemoved(EntryEvent<String, Note> event) {
                 System.out.println("Removed: " + event);
                 remove(event);
             }
-
+            
             @Override
             public void entryUpdated(EntryEvent<String, Note> event) {
                 System.out.println("Updated: " + event);
                 update(event);
             }
-
+            
             @Override
             public void entryEvicted(EntryEvent<String, Note> event) {
             }
         }, true);
-
+        
         refill();
     }
-
+    
     public void add(EntryEvent<String, Note> event) {
         final Note note = event.getValue();
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
                 added.set(note);
+                save(note);
             }
         });
     }
-
+    
     public void update(EntryEvent<String, Note> event) {
         final Note note = event.getValue();
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
                 updated.set(note);
+                save(note);
             }
         });
     }
-
+    
     public void remove(EntryEvent<String, Note> event) {
         final Note note = event.getValue();
         Platform.runLater(new Runnable() {
@@ -95,44 +97,44 @@ public class NotesStore {
             }
         });
     }
-
+    
     void refill() {
         for (Note note : notes.values()) {
             this.added.set(note);
         }
     }
-
+    
     public void create(Note note) {
         this.notes.put(note.titleProperty().get(), note);
         save();
     }
-
+    
     public void delete(Note note) {
         this.notes.remove(note);
     }
-
+    
     public ReadOnlyObjectProperty<Note> addedProperty() {
         return added;
     }
-
+    
     public ReadOnlyObjectProperty<Note> removedProperty() {
         return removed;
     }
-
+    
     public ReadOnlyObjectProperty<Note> updatedProperty() {
         return updated;
     }
-
+    
     public Collection<Note> allNotes() {
         return this.notes.values();
     }
-
+    
     public void save() {
         for (Note note : notes.values()) {
             save(note);
         }
     }
-
+    
     void save(Note note) {
         System.out.println("Saving: " + note);
         String title = note.titleProperty().get();
@@ -152,7 +154,7 @@ public class NotesStore {
             System.err.format("IOException: %s%n", ex);
         }
     }
-
+    
     public void update(Note note) {
         this.notes.put(note.titleProperty().get(), note);
     }
